7645092e0b784f04872b658c7a50c81f9d1bdeda
Yann Klein <yann.klein@me.com>
2021-04-30 00:11:13 +0900

    csv write part

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  @name = Character.all.first.name
+  new_character = Character.new("Vegeta#{Time.now}", "Sayan")
+  new_character.save
+  @name = Character.all.last.name
   erb :index
 end
--- models/character.rb
+++ models/character.rb
@@ -1,4 +1,5 @@
 require 'csv'
+FILEPATH = "#{File.dirname(__FILE__)}/../public/data.csv"
 
 class Character
   attr_reader :name, :species
@@ -8,6 +9,10 @@ class Character
     @species = species
   end
 
+  def save
+    Character.save_csv(self)
+  end
+
   def self.all
     Character.load_csv
   end
@@ -15,11 +20,23 @@ class Character
   def self.load_csv
     characters = []
     csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
-    filepath    = "#{File.dirname(__FILE__)}/../public/data.csv"
 
-    CSV.foreach(filepath, csv_options) do |row|
+    CSV.foreach(FILEPATH, csv_options) do |row|
       characters << Character.new(row['name'], row['species'])
     end
     characters
+  end
+
+  def self.save_csv(character)
+    characters = Character.load_csv
+    characters << character
+    csv_options = { col_sep: ',', force_quotes: true, quote_char: '"' }
+
+    CSV.open(FILEPATH, 'wb', csv_options) do |csv|
+      csv << ['name', 'species']
+      characters.each do |character|
+        csv << [character.name, character.species]
+      end
+    end
   end
 end
