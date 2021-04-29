2551b3e871648d36729b17cc16ac5a77faf0d3d2
Yann Klein <yann.klein@me.com>
2021-04-30 00:21:11 +0900

    create action

--- app.rb
+++ app.rb
@@ -3,8 +3,12 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  new_character = Character.new("Vegeta#{Time.now}", "Sayan")
-  new_character.save
-  @name = Character.all.last.name
+  @characters = Character.all
   erb :index
+end
+
+post '/create' do
+  new_character = Character.new(params['name'], params['species'])
+  new_character.save
+  redirect '/'
 end
